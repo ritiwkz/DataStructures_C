7cb4bc73ff03d7e97fc85330fc5ba468c04deb25
Ritik <hrr0077@gmail.com>
2022-09-28 17:53:52 +0530

    maximum and minimum of an Array

--- Array/min_maxArray.cpp
+++ Array/min_maxArray.cpp
@@ -13,5 +13,17 @@ int main()
     int n = sizeof(arr)/sizeof(arr[0]);
 
     // Print the orignal Array
+    cout<<"Entered Array = ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Minimum Element = "<< *min_element(arr,arr+n)<<endl;
+
+    cout<<"Maximum Element = "<< *max_element(arr, arr+n)<<endl;
+
+    return 0;
 
 }
