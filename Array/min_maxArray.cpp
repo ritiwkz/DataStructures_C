67652f0e65341f8f98c2aed6880580cac20c1751
Ritik <hrr0077@gmail.com>
2022-09-28 17:50:03 +0530

    Find the maximum and minimum of an Array

---
+++ Array/min_maxArray.cpp
@@ -0,0 +1,17 @@
+// C++ program to find the min and max element
+// of Array using sort() in STL
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Get the array
+    int arr[] = {12,3,23,1,24,57,77,3,4};
+
+    // Compute the size of the array
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    // Print the orignal Array
+
+}
