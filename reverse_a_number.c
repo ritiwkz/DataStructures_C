82e4346ec401b03c60c579d4ad7f4b685c74d96e
Ritik <hrr0077@gmail.com>
2022-10-06 21:03:33 +0530

    Reverse any Integer

--- reverse_a_number.c
+++ reverse_a_number.c
@@ -1,21 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-   int n, reverse = 0, rem;
+   int number, reverse = 0, rem;
 
    printf("Enter an integer: ");
-   scanf("%d",&n);
+   scanf("%d", &number);
 
-   while (n !=0)
+   while (number != 0)
    {
-    rem = n%10;
-    reverse = reverse*10 + rem;
-    n = n/10;
+      rem = number % 10;
+      reverse = reverse * 10 + rem;
+      number = number / 10;
    }
 
-   printf("Result = %d\n",reverse);
+   printf("Result = %d\n", reverse);
 
    return 0;
-   
 }
